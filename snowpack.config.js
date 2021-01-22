c4248c4287081e09df6517228eff637a566c6ecd
Alex Wei <weiyx93@gmail.com>
2021-01-22 11:08:34 +0800

    feat: setup react component

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,11 +1,12 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
+  // Customize the file structure of your project.
+  // The key is the name of the directory and the value is where you’d like them in the final build.
   mount: {
-    /* ... */
+    public: '/',
+    src: '/dist',
   },
-  plugins: [
-    /* ... */
-  ],
+  plugins: ['@snowpack/plugin-react-refresh'],
   routes: [
     /* Enable an SPA Fallback in development: */
     // {"match": "routes", "src": ".*", "dest": "/index.html"},
