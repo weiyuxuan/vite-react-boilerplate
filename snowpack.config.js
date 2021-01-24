6fc3938be5203cd027a4d212f29aed9c3209afe4
weiyuxuan <weiyx93@gmail.com>
2021-01-24 17:14:28 +0800

    chore: update snowpack.config.js

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,29 +1,47 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
-  // Customize the file structure of your project.
+  // Specify the root of a project using Snowpack.
+  root: './',
+
+  // Mount local directories to custom URLs in your built application.
   // The key is the name of the directory and the value is where you’d like them in the final build.
   mount: {
-    public: { url: '/', static: true },
-    src: { url: '/dist' },
+    public: { url: '/', static: true, resolve: false },
+    src: { url: '/assets' },
   },
+
+  // Configure import aliases for directories and packages.
+  alias: {
+    components: './src/components',
+  },
+
+  // Enable Snowpack plugins and their options.
   plugins: ['@snowpack/plugin-react-refresh', '@snowpack/plugin-sass'],
-  routes: [
-    /* Enable an SPA Fallback in development: */
-    // {"match": "routes", "src": ".*", "dest": "/index.html"},
-  ],
+
+  // Configure the Snowpack dev server.
+  devOptions: {
+    port: 3000,
+    hmr: true,
+    hmrErrorOverlay: true,
+  },
+
+  // Configure your final build.
+  buildOptions: {
+    out: 'dist',
+    baseUrl: '/',
+    sourcemap: false,
+  },
+
+  // Enable an SPA Fallback in development
+  routes: [],
+
+  // Using Snowpack's built-in bundling support.
   optimize: {
-    /* Example: Bundle your final build: */
     bundle: true,
     minify: true,
     target: 'es2020',
   },
-  packageOptions: {
-    /* ... */
-  },
-  devOptions: {
-    /* ... */
-  },
-  buildOptions: {
-    /* ... */
-  },
+
+  // Configure how npm packages are installed and used.
+  packageOptions: {},
 };
